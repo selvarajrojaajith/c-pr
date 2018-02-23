49be14b446252fd7611b4b9cbbe13bfe3ed0f912
selvarajrojaajith <36071813+selvarajrojaajith@users.noreply.github.com>
2018-02-23 15:54:49 +0530

    array.c
    nice

---
+++ array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int i,n;
+printf("\n Enter the number of an N integer");
+scanf("%d",&n,&i);
+{
+array=0;
+N==1;
+}
+printf("\n Enter the given integer");
+{
+for(i=0;i<2;i++);
+}
+return 0;
+}
